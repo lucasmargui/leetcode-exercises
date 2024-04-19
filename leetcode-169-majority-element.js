ab6ed05b529eb1eb93c39ef9f595dae21ba087de
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-04-19 12:15:06 -0300

    feat: add question 169

--- leetcode-169-majority-element.js
+++ leetcode-169-majority-element.js
@@ -1,72 +1,34 @@
-// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// Given an array nums of size n, return the majority element.
 
-// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
-// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ 
+
 // Example 1:
 
-// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-// Output: [1,2,2,3,5,6]
-// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+// Input: nums = [3,2,3]
+// Output: 3
 // Example 2:
 
-// Input: nums1 = [1], m = 1, nums2 = [], n = 0
-// Output: [1]
-// Explanation: The arrays we are merging are [1] and [].
-// The result of the merge is [1].
-// Example 3:
-
-// Input: nums1 = [0], m = 0, nums2 = [1], n = 1
-// Output: [1]
-// Explanation: The arrays we are merging are [] and [1].
-// The result of the merge is [1].
-// Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
 
 // Constraints:
 
-// nums1.length == m + n
-// nums2.length == n
-// 0 <= m, n <= 200
-// 1 <= m + n <= 200
-// -109 <= nums1[i], nums2[j] <= 109
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+ 
 
 /**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
+ * @param {number[]} nums
+ * @return {number}
  */
+ var majorityElement = function(nums) {
+    
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  return nums[Math.floor(n / 2)];
 
-function teste() {
-  var nums1 = [1, 2, 3, 0, 0, 0];
-  var nums2 = [2, 5, 6];
-  var m = 3;
-  var n = 3;
-
-  let i = m - 1; // index of last element in nums1
-  let j = n - 1; // index of last element in nums2
-  let k = m + n - 1; // index of last element in merged array
-
-  while (i >= 0 && j >= 0) {
-    if (nums1[i] > nums2[j]) {
-      console.log(nums1[i] + " > " + nums2[j]);
-      console.log("I é igual a " + i);
-      nums1[k--] = nums1[i--];
-    } else {
-      console.log(nums1[i] + " < " + nums2[j]);
-      console.log("J é igual a " + j);
-      nums1[k--] = nums2[j--];
-    }
-  }
-
-  // If there are remaining elements in nums2, they need to be copied into nums1
-  while (j >= 0) {
-    nums1[k--] = nums2[j--];
-  }
-
-  console.log(nums1);
-}
-
-teste();
+};
