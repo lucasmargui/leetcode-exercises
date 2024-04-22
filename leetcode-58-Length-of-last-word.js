9536e83ae85101cc3941562efbe7a970eeb03bc2
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-04-22 13:30:49 -0300

    feat: add question length last word

---
+++ leetcode-58-Length-of-last-word.js
@@ -0,0 +1,48 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal 
+// substring
+//  consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLastWord = function(s) {
+
+    var arraySeparated = s.split(' ');
+    var lastWord = 0;
+   
+
+ 
+   
+   arraySeparated.forEach(function (string) {
+       
+    if(string.length > 0){
+        lastWord = string.length;
+    }
+
+  });
+   
+   return lastWord;
+    
+};
