35d80ec07a189b8c048e8e29435bdf73d1364900
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-04-24 17:20:33 -0300

    feat: update code

--- leetcode-21-linked-list-merged.js
+++ leetcode-21-linked-list-merged.js
@@ -10,6 +10,21 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+///RECURSIVE 
+ var mergeTwoLists = function(l1, l2) {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  if (l1.val < l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+  } else {
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2;
+  }
+};
+
+
 
  var mergeTwoLists = function(l1, l2) {
   let tempNode = new ListNode(0, null);
