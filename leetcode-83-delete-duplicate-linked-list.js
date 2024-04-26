d109bf98162ecfda6d72218dca360bfa38837020
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-04-26 13:58:53 -0300

    feat: add question 83 duplicate linked list

---
+++ leetcode-83-delete-duplicate-linked-list.js
@@ -0,0 +1,39 @@
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+// Example 2:
+
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+ 
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var deleteDuplicates = function(head) {
+    let cur=head
+    while(cur && cur.next){
+        if(cur.val==cur.next.val){
+            cur.next=cur.next.next
+        }else{
+            cur=cur.next
+        }
+    }
+    return head
+};
