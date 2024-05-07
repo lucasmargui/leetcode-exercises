4e7a5c2136f4789fa4bf82850b9d77b22ffb5f26
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-05-07 16:20:57 -0300

    feat: update question

---
+++ leetcode-55-jump-game.js
@@ -0,0 +1,42 @@
+
+// You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+// Return true if you can reach the last index, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// 0 <= nums[i] <= 105
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var canJump = function(nums) {
+
+    let gas = 0;
+    for (let n of nums) {
+        if (gas < 0) {
+            return false;
+        } else if (n > gas) {
+            gas = n;
+        }
+        console.log(gas);
+        gas -= 1;
+    }
+    return true;
+ 
+};
