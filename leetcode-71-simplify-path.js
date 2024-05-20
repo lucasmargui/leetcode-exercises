1f1afd2cda2b586e7b2912b526c2d7b37ca82b2a
lucasmargui <lucasmargui@outlook.com>
2024-05-20 16:44:33 -0300

    feat: update question

--- leetcode-71-simplify-path.js
+++ leetcode-71-simplify-path.js
@@ -69,6 +69,27 @@
  * @param {string} path
  * @return {string}
  */
+
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    let stack = [];
+    path = path.split("/");
+    for(let i = 0; i < path.length; i++){
+        if(!path[i] || path[i] === ".") continue;
+        if(path[i] === ".."){
+            stack.pop();
+        } else {
+            stack.push(path[i]);
+        }
+    };
+    
+    return `/${stack.join("/")}`
+};
+
+
 var simplifyPath = function(path) {
 
     var stack = [];
