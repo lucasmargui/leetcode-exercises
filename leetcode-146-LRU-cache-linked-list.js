88536475c0de9acd9a296e75bcd0d53e3b3d05e1
lucasmargui <lucasmargui@outlook.com>
2024-05-22 17:21:05 -0300

    feat: update question

--- leetcode-146-LRU-cache-linked-list.js
+++ leetcode-146-LRU-cache-linked-list.js
@@ -1,46 +1,146 @@
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
 
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// Implement the LRUCache class:
 
-// You can return the answer in any order.
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+// int get(int key) Return the value of the key if the key exists, otherwise return -1.
+// void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+// The functions get and put must each run in O(1) average time complexity.
 
  
 
 // Example 1:
 
-// Input: nums = [2,7,11,15], target = 9
-// Output: [0,1]
-// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
+// Input
+// ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+// Output
+// [null, null, null, 1, null, -1, null, -1, 3, 4]
 
-// Input: nums = [3,2,4], target = 6
-// Output: [1,2]
-// Example 3:
-
-// Input: nums = [3,3], target = 6
-// Output: [0,1]
+// Explanation
+// LRUCache lRUCache = new LRUCache(2);
+// lRUCache.put(1, 1); // cache is {1=1}
+// lRUCache.put(2, 2); // cache is {1=1, 2=2}
+// lRUCache.get(1);    // return 1
+// lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+// lRUCache.get(2);    // returns -1 (not found)
+// lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+// lRUCache.get(1);    // return -1 (not found)
+// lRUCache.get(3);    // return 3
+// lRUCache.get(4);    // return 4
  
- var twoSum = function(nums, target) {
 
-  var i = 0;
-  var indices = [];
-  while(i < nums.length){
-      var x = i + 1;
+// Constraints:
 
-      while(x < nums.length){
-         
-          if(nums[i] + nums[x] == target){
-            indices = [i,x];
-            break;
-          
-         }
-         x++;
-      }
+// 1 <= capacity <= 3000
+// 0 <= key <= 104
+// 0 <= value <= 105
+// At most 2 * 105 calls will be made to get and put.
 
-      i++;   
+/**
+ * @param {number} capacity
 
-  }
 
-     return indices;
+ Problem
+ ========================
+ Create a LRU Cache
+ - `get` runs in O(1)
+ - `put` runs in O(1), evicts the least used key when capacity is reached
+ - has a capacity, set initially
+
+ Notes:
+ ------------------------
+ - Hashmap should be used for get, but need to store information about least recently used
+ - put and get should update lastRecentlyUsed
+
+ Playground
+ =======================
+ capacity = 3;
+ usage = {
+    test1: 2,
+    test2: 1,
+    test3: 1,
+ }
+ leastRecentlyUsed = [test1, test2, test3, test1]
  
+ hashmap = {
+    test1: 1,
+    test2: 2,
+    test3: 3
+ }
+ */
+ var LRUCache = function(capacity) {
+    this._cache = new Map();
+    this._capacity = capacity;
 };
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (!this._cache.has(key)) return -1;
+    const val = this._cache.get(key)
+    this._cache.delete(key);
+    this._cache.set(key, val)
+    return val;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    this._cache.delete(key);
+    this._cache.set(key, value)
+    if (this._cache.size > this._capacity) {
+        this._cache.delete(this._cache.keys().next().value)
+    }
+};
+
+
+
+
+var LRUCache = function(capacity) {
+    this.cache = new Map();
+    this.capacity = capacity;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+
+    if (!this.cache.has(key)) return -1;
+    const v = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, v);
+    return this.cache.get(key);
+
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+
+    if (this.cache.has(key)) {
+      this.cache.delete(key);
+    }
+    this.cache.set(key, value);
+    if (this.cache.size > this.capacity) {
+      this.cache.delete(this.cache.keys().next().value);  // keys().next().value returns first item's key
+    }
+    
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
