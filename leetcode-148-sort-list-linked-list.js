6fbcc433871417b6172a31da25111d2106cfb976
lucasmargui <lucasmargui@outlook.com>
2024-06-06 15:36:36 -0300

    feat: update question

--- leetcode-148-sort-list-linked-list.js
+++ leetcode-148-sort-list-linked-list.js
@@ -34,27 +34,72 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// var sortList = function(head) {
+
+//     var ordered = new ListNode(0);
+//     var orderedHead = ordered;
+//     var arr = [];
+
+//     while(head){
+//         arr.push(head.val);
+//         head = head.next;
+//     }
+
+//     arr.sort((a,b) => a - b);
+
+//      for (let i = 0; i < arr.length; i++){
+
+//          orderedHead.next = new ListNode(arr[i]);
+//          orderedHead = orderedHead.next;
+//      }
+
+//     return ordered.next
+
+    
+
+// };
+
+
+
+/**
+
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 var sortList = function(head) {
 
-    var ordered = new ListNode(0);
-    var orderedHead = ordered;
-    var arr = [];
-
-    while(head){
-        arr.push(head.val);
-        head = head.next;
+    function sortList (head) {
+        if(!head || !head.next) return head;
+        
+        // Middle of the linked list head
+        let fast = head, slow = head;
+        while(fast.next && fast.next.next) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        let middle = slow.next;
+        slow.next = null;
+        return merge(sortList(head), sortList(middle));
     }
-
-    arr.sort((a,b) => a - b);
-
-     for (let i = 0; i < arr.length; i++){
-
-         orderedHead.next = new ListNode(arr[i]);
-         orderedHead = orderedHead.next;
-     }
-
-    return ordered.next
-
-    
-    
-};
+    const merge = (o, t) => {
+        let d = new ListNode(-1); //dummy and temp
+        let tmp = d;
+        while(o && t) {
+            tmp.next = (o.val < t.val) ? o : t;
+            tmp = tmp.next;
+            if(o.val < t.val) o = o.next; else t = t.next;
+        }
+        if(o) tmp.next = o;
+        if(t) tmp.next = t;
+        return d.next;
+    }
+        
+        
+    };
