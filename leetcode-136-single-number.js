40ddc7794953c31ab8e9279f4b52654abcdf748b
lucasmargui <lucasmargui@outlook.com>
2024-06-10 19:04:18 -0300

    feat: update question

--- leetcode-136-single-number.js
+++ leetcode-136-single-number.js
@@ -1,93 +1,34 @@
-// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
 
-// You must implement a solution with a linear runtime complexity and use only constant extra space.
+  // If you analyze the truth table of XOR, also known as "Exclusive Or", 
+  // you will see that the output is true only when the inputs are different.
 
- 
+  // input a | input b | 
+  //   false | flase   | false
+  //   false | true    | true
+  //   true  | false   | true
+  //   true  | true    | false
 
-// Example 1:
-
-// Input: nums = [2,2,1]
-// Output: 1
-// Example 2:
-
-// Input: nums = [4,1,2,1,2]
-// Output: 4
-// Example 3:
-
-// Input: nums = [1]
-// Output: 1
- 
-
-// Constraints:
-
-// 1 <= nums.length <= 3 * 104
-// -3 * 104 <= nums[i] <= 3 * 104
-// Each element in the array appears twice except for one element which appears only once.
+  // 4    10 | 0
+  // ---------------
+  // 1    00 | 1  |    this columns all return zero because   
+  // 2    01 | 0  |    duplicates binary 0: 0 ^ 0 => 0
+  // 1    00 | 1  |    duplicates binary 1: 1 ^ 1 => 0
+  // 2    01 | 0  |    0 ^ 0 => 0
+  // ---------------
 
 
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-//  var singleNumber = function(nums) {
-//     for(let num of nums) {
-//         if(nums.indexOf(num) === nums.lastIndexOf(num)) return num;
-//     }
-// };
+  // results in binary comparation for unique number for 0
 
-
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-//  const singleNumber = (nums) => nums.reduce((acc, num) => acc ^ num, 0);
-
-
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-//  var singleNumber = function(nums) {
-//     let result = 0;
-
-//     for (const num of nums) {
-//         result ^= num;
-//     }
-
-//     return result;
-// };
-
-
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-//  var singleNumber = function(nums) {
-//     return nums[nums.findIndex(num => nums.filter(el => el === num).length === 1)]
-// };
-
-var singleNumber = function (nums) {
-  nums.sort();
-
-  var index = 0;
-
-  if (nums.length == 1) {
-    return nums[0];
-  }
-
-  if (index == 0 && nums[index] != nums[index + 1]) {
-    return nums[index];
-  }
-
-  if (nums[nums.length - 2] != nums[nums.length - 1]) {
-    return nums[nums.length - 1];
-  }
-
-  while (index < nums.length) {
-    if (nums[index] != nums[index + 1] && nums[index] != nums[index - 1]) {
-      return nums[index];
-    }
-
-    index++;
-  }
+   // Initialize the unique number...
+   let uniqNum = 0;
+   // TRaverse all elements through the loop...
+   for (let idx = 0; idx < nums.length; idx++) {
+       // Concept of XOR...
+       uniqNum = uniqNum ^ nums[idx];
+   } return uniqNum;       // Return the unique number...
 };
