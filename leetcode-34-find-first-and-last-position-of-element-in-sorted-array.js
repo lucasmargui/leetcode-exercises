2ec1668b339451c34f53e92940529495c1d8875e
lucasmargui <lucasmargui@outlook.com>
2024-06-10 13:43:38 -0300

    feat: update question

--- leetcode-34-find-first-and-last-position-of-element-in-sorted-array.js
+++ leetcode-34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -20,37 +20,101 @@
 // Output: [-1,-1]
 
 
+// var searchRange = (nums,target) => {
+//     let left = 0;
+//     let right = nums.length - 1;
+//     let first = -1;
+//     while(left <= right){
+//         let mid = Math.floor((left + right) / 2);
+
+//         if(nums[mid] === target){
+//             first = mid;
+//             right = mid - 1;
+//         }else if(nums[mid] < target){
+//             left = mid + 1
+//         }else{
+//             right = mid - 1
+//         }
+//     }
+
+//     left = 0;
+//     right = nums.length - 1;
+//     let last = -1;
+//     while(left <= right){
+//         let mid = Math.floor((left + right) / 2);
+
+//         if(nums[mid] === target){
+//             last = mid;
+//             left = mid + 1;
+//         }else if(nums[mid] < target){
+//             left = mid + 1;
+//         }else{
+//             right = mid - 1
+//         }
+//     }
+//     return [first,last];
+// }
+
+
 var searchRange = (nums,target) => {
-    let left = 0;
+
+    let left = 0
+    let leftFirst = left;
+    let leftLast = left;
     let right = nums.length - 1;
+    let rightFirst = right;
+    let rightLast = right;
+
     let first = -1;
-    while(left <= right){
-        let mid = Math.floor((left + right) / 2);
-
-        if(nums[mid] === target){
-            first = mid;
-            right = mid - 1;
-        }else if(nums[mid] < target){
-            left = mid + 1
-        }else{
-            right = mid - 1
-        }
-    }
-
-    left = 0;
-    right = nums.length - 1;
     let last = -1;
-    while(left <= right){
-        let mid = Math.floor((left + right) / 2);
 
-        if(nums[mid] === target){
-            last = mid;
-            left = mid + 1;
-        }else if(nums[mid] < target){
-            left = mid + 1;
+    while(leftFirst <= rightFirst || leftLast <= rightLast){
+
+
+        let mid = Math.floor((left+right)/2);
+
+        //1.Binary search until you find the target
+      
+        if(nums[mid] == target){
+
+           //2. When finding the value, perform 2 binary for the First and the Last 
+
+            let midFirst = Math.floor((leftFirst + rightFirst) / 2);
+            let midLast = Math.floor((leftLast + rightLast) / 2);
+
+            //Search first target in array
+            if(nums[midFirst] == target){
+                first = midFirst;
+                rightFirst = midFirst - 1;
+            }else{
+                leftFirst = midFirst + 1;
+            }
+
+            //Search first last target in array
+            if(nums[midLast] == target){
+                last = midLast;
+                leftLast = midLast + 1;
+            }else{
+                rightLast = midLast - 1;
+            }
+
+
+        }else if(target < nums[mid]){
+
+            right = mid - 1;
+            rightFirst = right;
+            rightLast = right;
+
         }else{
-            right = mid - 1
+            left = mid + 1;
+            leftFirst = left;
+            leftLast = left;
         }
+        
+
+
     }
-    return [first,last];
+
+    return [first,last]
+
 }
