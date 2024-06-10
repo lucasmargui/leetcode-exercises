ccc0a510f15351a0782bd5e6219c4bb50c5e8f69
lucasmargui <lucasmargui@outlook.com>
2024-06-10 15:34:59 -0300

    feat: add question

---
+++ leetcode-4-median-of-two-sorted-arrays-binary-search.js
@@ -0,0 +1,45 @@
+// Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+// Example 2:
+
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let merged = [];
+    let i = 0, j = 0;
+    
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] < nums2[j]) {
+            merged.push(nums1[i++]);
+        } else {
+            merged.push(nums2[j++]);
+        }
+    }
+
+    while (i < nums1.length) merged.push(nums1[i++]);
+    while (j < nums2.length) merged.push(nums2[j++]);
+
+    let mid = Math.floor(merged.length / 2);
+    if (merged.length % 2 === 0) {
+        return (merged[mid-1] + merged[mid]) / 2;
+    } else {
+        return merged[mid];
+    }
+};
