19a913d1a59608f1ce5b503137db97f1424fe9b0
lucasmargui <lucasmargui@outlook.com>
2024-06-20 14:24:06 -0300

    feat: update question

--- leetcode-72-edit-distance.dp.js
+++ leetcode-72-edit-distance.dp.js
@@ -30,29 +30,37 @@
 // exention -> exection (replace 'n' with 'c')
 // exection -> execution (insert 'u')
  
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
 var minDistance = function(word1, word2) {
 
-    const cache = Array.from({ length: word1.length + 1 }, () => Array(word2.length + 1).fill(Infinity));
+    const row = word1.length;
+    const col = word2.length;
+
+    const dp = Array.from({ length: row + 1 }, () => Array(col + 1).fill(Infinity));
     
-    for (let j = 0; j <= word2.length; j++) {
-        cache[word1.length][j] = word2.length - j;
+    for (let i = 0; i <= row; i++){
+        dp[i][col] = row - i;
     }
-    
-    for (let i = 0; i <= word1.length; i++) {
-        cache[i][word2.length] = word1.length - i;
+    for (let j = 0; j <= col; j++){
+        dp[row][j] = col - j;
     }
-    
-    for (let i = word1.length - 1; i >= 0; i--) {
-        for (let j = word2.length - 1; j >= 0; j--) {
-            if (word1[i] === word2[j]) {
-                cache[i][j] = cache[i + 1][j + 1];
-            } else {
-                cache[i][j] = 1 + Math.min(cache[i + 1][j], cache[i][j + 1], cache[i + 1][j + 1]);
+ 
+    for (let i = row - 1; i >= 0 ; i--){
+        for (let j = col - 1; j >= 0 ; j--){
+            if(word1[i] == word2[j]){
+                dp[i][j] = dp[i + 1][j + 1];
+            }else{
+                dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1]);
             }
         }
     }
     
-    return cache[0][0];
+   
+    return dp[0][0];
 
     
 
