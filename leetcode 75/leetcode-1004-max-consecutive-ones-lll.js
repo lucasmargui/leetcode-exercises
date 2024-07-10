6c2e88ff81be733a5578710b4e2df7c538fd9699
lucasmargui <lucasmargui@outlook.com>
2024-07-10 15:08:45 -0300

    feat: add question

---
+++ leetcode 75/leetcode-1004-max-consecutive-ones-lll.js
@@ -0,0 +1,42 @@
+
+// Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+// Output: 6
+// Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+// Example 2:
+
+// Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+// Output: 10
+// Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+
+
+    var leftWindow  = 0;
+    var rightWindow  = 0;
+
+    while( rightWindow < nums.length){
+        
+        if(nums[rightWindow] === 0) k--;
+        if(k < 0){
+            if(nums[leftWindow] === 0) k++;
+            leftWindow++;
+        }
+        rightWindow++;
+    }
+
+    return rightWindow - leftWindow;
+    
+};
--- leetcode 75/leetcode-643-maximum-average-subarray.js
+++ leetcode 75/leetcode-643-maximum-average-subarray.js
@@ -44,3 +44,5 @@ var findMaxAverage = function(nums, k) {
     return sumTotal
     
 };
+
+
