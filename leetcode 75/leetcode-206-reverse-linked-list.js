983dca429a2004feeef276ca2256f332b6eaf5d5
lucasmargui <lucasmargui@outlook.com>
2024-07-15 14:54:01 -0300

    feat: add question

---
+++ leetcode 75/leetcode-206-reverse-linked-list.js
@@ -0,0 +1,38 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+
+    if(head) retu
+
+    var dummy = null;
+
+
+    while(head){
+
+        let reverselist = new ListNode(head.val,dummy);
+        dummy = reverselist   
+        head = head.next;
+    }
+
+    return dummy
+};
