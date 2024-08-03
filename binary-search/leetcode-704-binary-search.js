b39f41886a407abc78aa31ccdc00278aa201d695
lucasmargui <lucasmargui@outlook.com>
2024-08-03 16:46:21 -0300

    feat: add question

---
+++ binary-search/leetcode-704-binary-search.js
@@ -0,0 +1,50 @@
+// 704. Binary Search
+// Solved
+// Easy
+// Topics
+// Companies
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+// Example 2:
+
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+    var left = 0;
+    var right = nums.length - 1;
+
+    while(left <= right){
+
+        let mid = Math.floor((left + right) / 2);
+
+        if(target == nums[mid]){
+            return mid;
+        }else if(target < nums[mid]){
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+
+    }
+
+    return -1;
+
+    
+};
